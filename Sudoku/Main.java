b17bb3dc706791d8c7b803d268e3eeba3199a1fa
Bharathlakshmi <bharathlakshmiselvaraj11@gmail.com>
2025-01-13 11:40:27 +0530

    create Main class

---
+++ Sudoku/Main.java
@@ -0,0 +1,59 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+package sudoku;
+public class Main {
+
+    public static void main(String[] args) {
+        int[][] board = {
+                {5, 3, 0, 0, 7, 0, 0, 0, 0},
+                {6, 0, 0, 1, 9, 5, 0, 0, 0},
+                {0, 9, 8, 0, 0, 0, 0, 6, 0},
+                {8, 0, 0, 0, 6, 0, 0, 0, 3},
+                {4, 0, 0, 8, 0, 3, 0, 0, 1},
+                {7, 0, 0, 0, 2, 0, 0, 0, 6},
+                {0, 6, 0, 0, 0, 0, 2, 8, 0},
+                {0, 0, 0, 4, 1, 9, 0, 0, 5},
+                {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+
+        SudokuBoard sb=new SudokuBoard(board);
+        System.out.println("--------------------------------");
+        System.out.println("Puzzle");
+        sb.print();
+
+        System.out.println("--------------------------------");
+        System.out.println("\n" + "Solution");
+        sb.solve(0,0);
+        sb.print();
+        System.out.println("--------------------------------");
+
+    }
+}
+
+ /*
+
+package sudoku;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] board = {
+                {5, 3, 0, 0, 7, 0, 0, 0, 0},
+                {6, 0, 0, 1, 9, 5, 0, 0, 0},
+                {0, 9, 8, 0, 0, 0, 0, 6, 0},
+                {8, 0, 0, 0, 6, 0, 0, 0, 3},
+                {4, 0, 0, 8, 0, 3, 0, 0, 1},
+                {7, 0, 0, 0, 2, 0, 0, 0, 6},
+                {0, 6, 0, 0, 0, 0, 2, 8, 0},
+                {0, 0, 0, 4, 1, 9, 0, 0, 5},
+                {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+        SudokuBoard sudokuBoard = new SudokuBoard(board);
+        sudokuBoard.print();
+        System.out.println("-------------------------------------------");
+        sudokuBoard.solve(0, 0);
+        sudokuBoard.print();
+    }
+}
+
+         */
